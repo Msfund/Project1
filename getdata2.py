c000a2b3f003029041342a0242089290af766966
shiyie <38480974+shiyie@users.noreply.github.com>
2018-04-19 13:18:44 +0800

    Update getdata2.py
    调整主力合约和次力合约筛选部分

--- getdata2.py
+++ getdata2.py
@@ -61,112 +61,117 @@ class GetFutureData:
         delistdate.columns = [i[0] for i in self.cursor.description]
         return delistdate
 #------------------------------------------------------------------------------
-
     def get_dom_data(self):
 
         trade_data = self.get_trade_data()
-        trade_data['S_INFO_CODE']=pd.DataFrame([i[2:6] for i in trade_data['S_INFO_WINDCODE']])
         trade_data.sort_values(by = ['TRADE_DT','S_INFO_WINDCODE','S_DQ_OI'], ascending = [1,1,0], inplace = True)
         maxOI = trade_data.groupby('TRADE_DT').nth(0)
         maxOI = maxOI.reset_index()
-# 确认合约切换点
-        loca = ~(maxOI['S_INFO_CODE'] == maxOI['S_INFO_CODE'].shift(1))
-# 条件1：持仓量连续第一满3天
+        maxOI1 = maxOI.copy()
+        maxOI['S_INFO_WINDCODE1']=None
+        #因为切换至下一合约需要3天判断期，主力合约名向后移动3天
+        maxOI['S_INFO_WINDCODE'] = maxOI['S_INFO_WINDCODE'].shift(3)
+        maxOI['S_INFO_WINDCODE'] = maxOI['S_INFO_WINDCODE'].fillna(method = 'bfill')
+        # 确认合约切换点位置
+        loca = ~(maxOI['S_INFO_WINDCODE'] == maxOI['S_INFO_WINDCODE'].shift(1))
+        # 条件1：持仓量连续第一满3天
         # 找出持仓量最大的合约,判断是否为连续三天，这里check1为标记连续持仓量最大达三天
-        check1 = (maxOI['S_INFO_CODE'] == maxOI['S_INFO_CODE'].shift(-1)) & (maxOI['S_INFO_CODE'] == maxOI['S_INFO_CODE'].shift(-2))
-# 条件2：金融期货不会往回切换（比如12月先作为主力合约，那么11月不能成为主力合约）
+        check1 = (maxOI['S_INFO_WINDCODE'] == maxOI['S_INFO_WINDCODE'].shift(-1)) & (maxOI['S_INFO_WINDCODE'] == maxOI['S_INFO_WINDCODE'].shift(-2))
+        #输入满足条件1的换仓位
+        maxOI['S_INFO_WINDCODE1'].ix[(loca)&(check1)] = maxOI['S_INFO_WINDCODE'].ix[(loca)&(check1)]
+        #向后填充
+        maxOI['S_INFO_WINDCODE1'] = maxOI['S_INFO_WINDCODE1'].fillna(method = 'ffill')
+        
+        # 条件2：金融期货不会往回切换（比如12月先作为主力合约，那么11月不能成为主力合约）
         # if语句中判断是否往回切换，这里check2为标记出现往回切换的合约
         if self.define_ExchMarktList() == 'filesync.CIndexFuturesEODPrices':
-            for i in range(len(maxOI['S_INFO_CODE'])):
+            for i in range(len(maxOI['S_INFO_WINDCODE'])):
                 if i == 0:
                     check2 = [False,]
                 else:
-                    check2.append(maxOI['S_INFO_CODE'][i]<maxOI['S_INFO_CODE'][:i].max())
-        check2 = pd.Series(check2)
-# 条件3：主力合约切换时不会向当月合约切换
+                    check2.append(maxOI['S_INFO_WINDCODE'][i]<maxOI['S_INFO_WINDCODE'][:i].max())
+            check2 = pd.Series(check2)
+            #调试满足条件2
+            maxOI['S_INFO_WINDCODE1'].ix[check2]=None
+            #向前填充，主力合约向后递延
+            maxOI['S_INFO_WINDCODE1'] = maxOI['S_INFO_WINDCODE1'].fillna(method = 'bfill')
+            
+        # 条件3：主力合约切换时不会向当月合约切换
         # 判断持仓量最大的合约是否为当月合约，这里chexk3标记当月合约的主力合约
-        temp = (pd.DataFrame([i for i in maxOI['S_INFO_CODE']]) == pd.DataFrame([i[-6:-2] for i in maxOI['TRADE_DT']]))
+        temp = (pd.DataFrame([i for i in maxOI['S_INFO_WINDCODE']]) == pd.DataFrame([i[-6:-2] for i in maxOI['TRADE_DT']]))
         temp = np.array(temp)
         temp = temp.tolist()
         for i in range(len(temp)):
             if i == 0:
-                check3 = [True,]
+                check3 = [False,]
             else:
                 check3.append(temp[i][0])
         check3 = pd.Series(check3)
-
-#  条件4：如果出现合约退市仍作为主力合约的情况，那么顺延到下一个主力合约
-#   感觉不需要这部分,因为有交易数据的部分都不会退市
-        delistdate = self.future_delistdate()
-        delistdate['S_INFO_CODE']=pd.DataFrame([i[2:6] for i in delistdate['S_INFO_WINDCODE']])
-        maxOI = pd.merge(delistdate,maxOI,how = 'right',on = ['S_INFO_WINDCODE'])
-        check4 = (maxOI['S_DQ_CLOSE'].astype(str) == 'nan')
-# =============================================================================
-# 条件合并
-        #输入满足条件1的换仓位
-        maxOI['S_INFO_WINDCODE1']=None
-        maxOI['S_INFO_WINDCODE1'].ix[(loca)&(check1)] = maxOI['S_INFO_WINDCODE'].ix[(loca)&(check1)]
-        #向后填充
-        maxOI['S_INFO_WINDCODE1'] = maxOI['S_INFO_WINDCODE1'].fillna(method = 'ffill')
-        #调试满足条件2
-        maxOI['S_INFO_WINDCODE1'].ix[check2]=None
-        #向前填充，主力合约向后递延
-        maxOI['S_INFO_WINDCODE1'] = maxOI['S_INFO_WINDCODE1'].fillna(method = 'bfill')
         #调试满足条件3
         maxOI['S_INFO_WINDCODE1'].ix[loca & check3]=None
         #向前填充，主力合约向后递延
         maxOI['S_INFO_WINDCODE1'] = maxOI['S_INFO_WINDCODE1'].fillna(method = 'bfill')
+        #  条件4：如果出现合约退市仍作为主力合约的情况，那么顺延到下一个主力合约
+        delistdate = self.future_delistdate()
+        maxOI = pd.merge(delistdate,maxOI,how = 'right',on = ['S_INFO_WINDCODE'])
+        check4 = (maxOI['TRADE_DT']>maxOI['S_INFO_DELISTDATE'])
         #调试满足条件4
         maxOI['S_INFO_WINDCODE1'].ix[check4]=None
         #向前填充，主力合约向后递延
         maxOI['S_INFO_WINDCODE1'] = maxOI['S_INFO_WINDCODE1'].fillna(method = 'bfill')
-        dom_data = maxOI
-
+# 整理数据
+        del maxOI['S_INFO_WINDCODE']
+        maxOI = maxOI.rename(columns = {'S_INFO_WINDCODE1':'S_INFO_WINDCODE'}) 
+        dom_data = pd.merge(maxOI[['S_INFO_WINDCODE','TRADE_DT']],maxOI1,how = 'left', on = ['S_INFO_WINDCODE','TRADE_DT'])
         dom_data = self.get_limit_data(dom_data)
+        dom_data = self.get_MarginRatio(dom_data)
         dom_data = self.get_adj_factor(dom_data)
         return dom_data
-
+#---------------------------------------------------------------------------
     def get_sub_data(self):
 
         trade_data = self.get_trade_data()
-        trade_data['S_INFO_CODE']=pd.DataFrame([i[2:6] for i in trade_data['S_INFO_WINDCODE']])
         trade_data.sort_values(by = ['TRADE_DT','S_INFO_WINDCODE','S_DQ_OI'], ascending = [1,1,0], inplace = True)
         subOI = trade_data.groupby('TRADE_DT').nth(1)
         subOI = subOI.reset_index()
-# 确认合约切换点
-        loca = ~(subOI['S_INFO_CODE'] == subOI['S_INFO_CODE'].shift(1))
-# 条件1：持仓量连续第一满3天
+        subOI1 = subOI.copy()
+        subOI['S_INFO_WINDCODE2']=None
+        #因为切换至下一合约需要3天判断期，主力合约名向后移动3天
+        subOI['S_INFO_WINDCODE'] = subOI['S_INFO_WINDCODE'].shift(3)
+        subOI['S_INFO_WINDCODE'] = subOI['S_INFO_WINDCODE'].fillna(method = 'bfill')
+        # 确认合约切换点位置
+        loca = ~(subOI['S_INFO_WINDCODE'] == subOI['S_INFO_WINDCODE'].shift(1))
+        # 条件1：持仓量连续第一满3天
         # 找出持仓量最大的合约,判断是否为连续三天，这里check1为标记连续持仓量最大达三天
-        check1 = (subOI['S_INFO_CODE'] == subOI['S_INFO_CODE'].shift(-1)) & (subOI['S_INFO_CODE'] == subOI['S_INFO_CODE'].shift(-2))
-# 条件2：主力次主力合约不能为同一张合约
-        dom_data = self.get_dom_data()
-        subOI = pd.merge(dom_data[['S_INFO_WINDCODE1','TRADE_DT']],subOI,how = 'right',on = ['TRADE_DT'])
-        check2 = subOI['S_INFO_WINDCODE']==subOI['S_INFO_WINDCODE1']
-
-# # 条件3：如果出现合约退市仍作为主力合约的情况，那么顺延到下一个主力合约
-#   感觉不需要这部分,因为有交易数据的部分都不会退市
-        delistdate = self.future_delistdate()
-        delistdate['S_INFO_CODE']=pd.DataFrame([i[2:6] for i in delistdate['S_INFO_WINDCODE']])
-        delistdate=delistdate.rename(columns = {'S_INFO_WINDCODE':'S_INFO_WINDCODEx'})
-        subOI = pd.merge(delistdate[['S_INFO_WINDCODEx','S_INFO_CODE','S_INFO_LISTDATE','S_INFO_DELISTDATE']],subOI,how = 'right',on = ['S_INFO_CODE'])
-        check3 = (subOI['S_DQ_CLOSE'].astype(str) == 'nan')
-# =============================================================================
-# 条件合并
+        check1 = (subOI['S_INFO_WINDCODE'] == subOI['S_INFO_WINDCODE'].shift(-1)) & (subOI['S_INFO_WINDCODE'] == subOI['S_INFO_WINDCODE'].shift(-2))
         #输入满足条件1的换仓位
-        subOI['S_INFO_WINDCODE1']=None
-        subOI['S_INFO_WINDCODE1'].ix[(loca)&(check1)] = subOI['S_INFO_WINDCODE'].ix[(loca)&(check1)]
+        subOI['S_INFO_WINDCODE2'].ix[(loca)&(check1)] = subOI['S_INFO_WINDCODE'].ix[(loca)&(check1)]
         #向后填充
-        subOI['S_INFO_WINDCODE1'] = subOI['S_INFO_WINDCODE1'].fillna(method = 'ffill')
+        subOI['S_INFO_WINDCODE2'] = subOI['S_INFO_WINDCODE2'].fillna(method = 'ffill')
+        # 条件2：主力次主力合约不能为同一张合约
+        dom_data = self.get_dom_data()
+        dom_data = dom_data.rename(columns = {'S_INFO_WINDCODE':'S_INFO_WINDCODE1'})
+        subOI = pd.merge(dom_data[['S_INFO_WINDCODE1','TRADE_DT']],subOI,how = 'right',on = ['TRADE_DT'])
+        check2 = subOI['S_INFO_WINDCODE1']==subOI['S_INFO_WINDCODE']
         #调试满足条件2
-        subOI['S_INFO_WINDCODE1'].ix[check2]=None
+        subOI['S_INFO_WINDCODE2'].ix[check2]=None
         #向前填充，主力合约向后递延
-        subOI['S_INFO_WINDCODE1'] = subOI['S_INFO_WINDCODE1'].fillna(method = 'bfill')
+        subOI['S_INFO_WINDCODE2'] = subOI['S_INFO_WINDCODE2'].fillna(method = 'bfill')
+# # 条件3：如果出现合约退市仍作为主力合约的情况，那么顺延到下一个主力合约
+#        delistdate = a.future_delistdate()
+        delistdate = self.future_delistdate()
+        subOI = pd.merge(delistdate[['S_INFO_WINDCODE','S_INFO_LISTDATE','S_INFO_DELISTDATE']],subOI,how = 'right',on = ['S_INFO_WINDCODE'])
+        check3 = (subOI['TRADE_DT']>subOI['S_INFO_DELISTDATE'])
         #调试满足条件3
-        subOI['S_INFO_WINDCODE1'].ix[check3]=None
+        subOI['S_INFO_WINDCODE2'].ix[check3]=None
         #向前填充，主力合约向后递延
-        subOI['S_INFO_WINDCODE1'] = subOI['S_INFO_WINDCODE1'].fillna(method = 'bfill')
-        sub_data = subOI
-
+        subOI['S_INFO_WINDCODE2'] = subOI['S_INFO_WINDCODE2'].fillna(method = 'bfill')
+# 整理数据 
+        del subOI['S_INFO_WINDCODE']
+        subOI = subOI.rename(columns = {'S_INFO_WINDCODE2':'S_INFO_WINDCODE'})
+        sub_data = pd.merge(subOI[['S_INFO_WINDCODE','TRADE_DT']],subOI1,how = 'left', on = ['S_INFO_WINDCODE','TRADE_DT'])
+        
+        sub_data = self.get_MarginRatio(sub_data)
         sub_data = self.get_limit_data(sub_data)
         sub_data = self.get_adj_factor(sub_data)
         return sub_data
