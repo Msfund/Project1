856306bdeb0b0f923526cc40d75fd93f8644105d
xyshell <38464985+xyshell@users.noreply.github.com>
2018-04-21 13:58:18 +0800

    none

--- getdata2.py
+++ getdata2.py
@@ -25,8 +25,7 @@ class GetFutureData:
         else:
             exchmarkt = 'filesync.CCommodityFuturesEODPrices'
         sql = '''select s_info_windcode,trade_dt,
-        s_dq_presettle,s_dq_open,s_dq_high,s_dq_low,s_dq_close,s_dq_volume,s_dq_oi
-        from '''+exchmarkt+'''
+        s_dq_presettle,s_dq_open,s_dq_high,s_dq_low,s_dq_close,s_dq_volume,s_dq_oi from '''+exchmarkt+'''
         where trade_dt>= '''+self.startdate+' and trade_dt<= '+self.enddate+"and s_info_windcode LIKE '"+self.vt+'''%'
         and LENGTH(s_info_windcode)>=9
         order by trade_dt'''
