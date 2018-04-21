1b58e6cc8243b51fb1457672c7ff3132b1afcb48
xyshell <38464985+xyshell@users.noreply.github.com>
2018-04-21 18:14:09 +0800

    none

--- getdata3.py
+++ getdata3.py
@@ -4,6 +4,11 @@ import numpy as np
 import h5py
 import re
 
+CFEcode = ('IF','IC','IH','TF','T')
+SHFcode = ('cu','al','zn','ru','fu','au','ag','rb','wr','pb','bu','hc','NI','SN')
+DCEcode = ('a','b','m','c','y','p','l','v','j','jm','i','jd','fb','bb','pp','cs')
+CZCcode = ('PM','WH','CF','SR','OI','TA','RI','LR','MA','FG','RS','RM','TC','ZC','JR','SF','SM')
+Allcode = CFEcode + SHFcode + DCEcode + CZCcode
 # CCommodityFuturesEODPrices：date vt preSettle open high low close volumn openinterest
 class GetFutureData:
 
@@ -14,183 +19,72 @@ class GetFutureData:
         db = cx_Oracle.connect('fe','fe','192.168.100.22:1521/winddb')
         self.cursor = db.cursor()
 
-    def define_ExchMarktList(self):
-        '''定义不同品种所属列表'''
-        exchmarkt = {}
-        exchmarkt['CFE'] = 'filesync.CIndexFuturesEODPrices'         # 中金所
-        exchmarkt['SHF'] = 'filesync.CCommodityFuturesEODPrices'     # 上期所
-        exchmarkt['CZC'] = 'filesync.CCommodityFuturesEODPrices'     # 郑商所
-        exchmarkt['DCE'] = 'filesync.CCommodityFuturesEODPrices'     # 大商所
-        sql = '''select  s_info_windcode
-        from filesync.CFuturesDescription '''"where s_info_windcode LIKE'"+self.vt+'''%' and rownum=1'''
-        self.cursor.execute(sql)
-        t = self.cursor.fetchall()
-        return exchmarkt[t[0][0][-3:]]  #例如'IF1003.CFE'返回的是exchmarkt['CFE']的值
-
     def get_trade_data(self):
-       '''读取日交易数据'''
-       sql = '''select s_info_windcode,
-       trade_dt,
-       s_dq_presettle,
-       s_dq_open,
-       s_dq_high,
-       s_dq_low,
-       s_dq_close,
-       s_dq_volume,
-       s_dq_oi from '''+self.define_ExchMarktList()+'''
-       where trade_dt>= '''+self.startdate+' and trade_dt<= '+self.enddate+"and s_info_windcode LIKE'"+self.vt+'''%'
-       and LENGTH(s_info_windcode)>=10
-       order by trade_dt'''
-       self.cursor.execute(sql)
-       trade_data = self.cursor.fetchall()
-       trade_data = pd.DataFrame(trade_data)
-       trade_data.columns = [i[0] for i in self.cursor.description]
-       trade_data = trade_data.sort_values(by = ['TRADE_DT','S_INFO_WINDCODE'])
-       return trade_data
+        if self.vt in CFEcode:
+            exchmarkt = 'filesync.CIndexFuturesEODPrices'
+        else:
+            exchmarkt = 'filesync.CCommodityFuturesEODPrices'
+        sql = '''select s_info_windcode,trade_dt,
+        s_dq_presettle,s_dq_open,s_dq_high,s_dq_low,s_dq_close,s_dq_volume,s_dq_oi from '''+exchmarkt+'''
+        where trade_dt>= '''+self.startdate+' and trade_dt<= '+self.enddate+"and s_info_windcode LIKE '"+self.vt+'''%'
+        and LENGTH(s_info_windcode)>=9
+        order by trade_dt'''
+        self.cursor.execute(sql)
+        trade_data = self.cursor.fetchall()
+        trade_data = pd.DataFrame(trade_data)
+        trade_data.columns = [i[0] for i in self.cursor.description]
+        trade_data = trade_data.sort_values(by = ['TRADE_DT','S_INFO_WINDCODE'])
+        return trade_data
 
-    def get_dom_data(self):
-
+    def get_code(self):
         trade_data = self.get_trade_data()
-        trade_data.sort_values(by = ['TRADE_DT','S_INFO_CODE','S_DQ_OI'], ascending = [1,1,0], inplace = True)
-        # 找出持仓量最大的合约,判断是否为连续三天
-        maxvol = trade_data.groupby('TRADE_DT')['S_INFO_CODE'].nth(1)
-        maxvol.check1 = maxvol['S_INFO_CODE'] == maxvol['S_INFO_CODE'].shift(1) and \
-                           maxvol['S_INFO_CODE'] == maxvol['S_INFO_CODE'].shift(2)
-        # 判断持仓量最大的合约是否为当月合约
-        maxvol.check2 = maxvol['S_INFO_CODE'].month() != maxvol['TRADE_DT'].month()
+        trade_sort = trade_data.sort_values(by = ['TRADE_DT','S_DQ_OI','S_INFO_WINDCODE'], ascending = [1,0,1])
+        maxOI = trade_sort.groupby('TRADE_DT').nth(0).reset_index()[['TRADE_DT','S_INFO_WINDCODE']]
+        subOI = trade_sort.groupby('TRADE_DT').nth(1).reset_index()[['TRADE_DT','S_INFO_WINDCODE']]
+        thiOI = trade_sort.groupby('TRADE_DT').nth(2).reset_index()[['TRADE_DT','S_INFO_WINDCODE']]
+        dom_code = maxOI
+        sub_code = subOI
+        if self.vt in CFEcode:
+            # 金融期货直接按maxOI，subOI，且不回滚 ps:windcode&code
+            sub_code[maxOI > subOI] = thiOI
+        else:
+            # 商品期货满足最大持仓量满3天
+            FSlag1check = subOI.shift(1) == maxOI
+            FSlag2check = subOI.shift(2) == maxOI
+            FScheck = [FSlag1check,FSlag2check].any(axis=1)
+            dom_code[FScheck] = None
+            dom_code = dom_code.fillna(method = 'ffill')
+            STlag1check = thiOI.shift(1) == subOI
+            STlag2check = thiOI.shift(1) == subOI
+            STcheck = [STlag1check,STlag2check].any(axis=1)
+            dom_code[STcheck] = None
+            sub_code = sub_code.fillna(method = 'ffill')
+        dom_code = dom_code.get_adj_factor(dom_code)
+        sub_code = sub_code.get_adj_factor(sub_code)
+        return dom_code sub_code
 
-        dom_code = pd.DataFrame({})
-        for i in range(1,len(trade_data.index)):
-            dom_code(i) = maxvol['S_INFO_CODE'](i) if i == 1 else pass
-            dom_code(i) = dom_code(i-1) ###copy
-            if dom_code(i) != maxvol['S_INFO_CODE'](i) and maxvol.check = True and maxvol.check2 = True:
-                dom_code(i) =  maxvol['S_INFO_CODE'](i)
+    def get_adj_factor(self,code):
+        lead = code['S_INFO_WINDCODE'].shift(-1) != code['S_INFO_WINDCODE']
+        lag = code['S_INFO_WINDCODE'].shift(1) != code['S_INFO_WINDCODE']
+        temp = code[lead].merge(code['S_INFO_WINDCODE'][lag])
+        ### rename 'S_INFO_WINDCODE_x'=old 'S_INFO_WINDCODE_y'=new
+        temp.merge(self.trade_data['S_DQ_CLOSE'],left_on='old',right_on='S_INFO_WINDCODE',inplace=True)
+        temp.merge(self.trade_data['S_DQ_CLOSE'],left_on='new',right_on='S_INFO_WINDCODE',inplace=True)
+        ### rename 'S_DQ_CLOSE_x'=oldclose 'S_DQ_CLOSE_y'=newclose
+        temp['adj_factor'] = temp['oldclose'] / temp['newclose']
+        code['adj_factor'] = None
+        code[lag]['adj_factor'] = temp['adj_factor']
+        code = code.fillna(methond = 'ffill')
+        code = code.fillna(value = 1) # 第一个调整因子为1
+        return code
 
-        dom_data = pd.merge(dom_code, trade_data, how='left join', on='S_INFO_CODE') ###
-        return dom_data
-
-    def get_subdom_data(self):
-
-        trade_data = get_trade_data()
-        # 找出持仓量次大的合约,判断是否为连续三天
-        subvol = trade_data.groupby('TRADE_DT')['S_INFO_CODE'].nth(2)
-        subvol.check1 = subvol['S_INFO_CODE'] == subvol['S_INFO_CODE'].shift(1) and \
-                           subvol['S_INFO_CODE'] == subvol['S_INFO_CODE'].shift(2)
-
-        subdom_code = pd.DataFrame({})
-        for i in range(1,len（trade_data.index)):
-            subdom_code(i) = subvol['S_INFO_CODE'](i) if i == 1 else pass
-            subdom_code(i) = dom_code(i-1) ###copy
-            if subdom_code() != subvol['S_INFO_CODE'](i):
-                subdom_code(i) = subvol['S_INFO_CODE'](i)
-
-        subdom_data = pd.merge(subdom_code, trade_data, how='left join', on='S_INFO_CODE') ###
-        return subdom_data
-
-#--------------------------------------------------------------------------------
-    def get_MarginRatio(self,data):
-        ## 获取保证金数据
-        sql = '''select
-        s_info_windcode,
-        marginratio,
-        trade_dt
-        from filesync.CFuturesmarginratio
-        '''"where s_info_windcode LIKE'"+self.vt+'''%'
-        order by s_info_windcode'''
-        self.cursor.execute(sql)
-        Margin_data = pd.DataFrame(self.cursor.fetchall())
-        Margin_data.columns = [i[0] for i in self.cursor.description]
-        data = data.merge(Margin_data, on = ['S_INFO_WINDCODE','TRADE_DT'],how = 'left')
-        data['MARGINRATIO'] = data['MARGINRATIO'].fillna(method = 'ffill')
-        if sum(data.isnull()['MARGINRATIO']) != 0:
-            #标记未找到对应保证金数据的合约位置
-            null_contract = data.isnull()
-            list_null = data['S_INFO_WINDCODE'].ix[null_contract['MARGINRATIO']]
-            list_uniq = list_null[~list_null.duplicated()]
-            #找出一共多有多少品种无对应数据，以及输出品种合约名和其对应保证金数据
-            special_Margin = self.special_MarginRatio(list_uniq)
-            for i in range(len(special_Margin)):
-                data['MARGINRATIO'][list_uniq.index[i]] = special_Margin['MARGINRATIO'][i] #这里只对应品种第一次出现的缺失数据项
-            #再次填充
-            data['MARGINRATIO'] = data['MARGINRATIO'].fillna(method = 'ffill')
-        return data
-#------------------------------------------------------------------------------
-    def special_MarginRatio(self,list_uniq):
-        '''如果合约找不到对应保证金，从CFuturescontpro表中再次查询'''
-        '''list_uniq为series格式'''
-        list_uniq = str(tuple(list_uniq.tolist()))
-        sql='''select s_info_windcode,
-        s_info_ftmargins
-        from filesync.CFuturescontpro
-        where s_info_windcode in'''+list_uniq+'''order by s_info_windcode'''
-        self.cursor.execute(sql)
-        special_Margin=pd.DataFrame(list(self.cursor.fetchall()))
-        special_Margin[2] = [int(re.findall(r"\d+\.?\d*",special_Margin.iloc[i,1])[0]) for i in range(len(special_Margin))]
-        del special_Margin[1]
-        special_Margin.columns=['S_INFO_WINDCODE','MARGINRATIO']
-        return special_Margin
-
-    def get_limit_data(self,data):    # 获取 uplimit 和 downlimit
-        sql = '''select s_info_windcode,
-        s_info_maxpricefluct
-        from filesync.CFuturescontpro
-        '''"where s_info_windcode LIKE'"+self.vt+'''%'
-        order by s_info_windcode'''
-        self.cursor.execute(sql)
-        limit_info = self.cursor.fetchall()
-        limit_info = pd.DataFrame(limit_info)
-        limit_info[2] = [int(re.findall(r"\d+\.?\d*",limit_info.iloc[i,1])[0]) for i in range(len(limit_info))]
-        del limit_info[1]
-        limit_info.columns=['S_INFO_WINDCODE','limit_ratio']
-        data = data.merge(limit_info,on='S_INFO_WINDCODE',how='left')
-        data['uplimit'] = data['S_DQ_PRESETTLE']*(1+0.01*data['limit_ratio'])
-        data['downlimit'] = data['S_DQ_PRESETTLE']*(1-0.01*data['limit_ratio'])
-        return data
-#----------------------------------------------------------------------------------
-    def get_adj_factor(self,data):
-        # 计算调整因子
-        loc = data['S_INFO_WINDCODE'].shift(-1) != data['S_INFO_WINDCODE']
-        loc.iloc[-1] = False
-        loc = loc.index[loc]
-        old = data.ix[loc,]
-        new = data.ix[loc+1,]
-        new.index = range(len(new))
-        old.index = range(len(old))
-        code_first = old['S_INFO_WINDCODE'].iloc[0]
-        day_first = data['TRADE_DT'].iloc[0]
-        date_record =  new['TRADE_DT'].copy()
-        date_record.index = range(1,(len(date_record)+1))
-
-        new = new[['S_INFO_WINDCODE','TRADE_DT']]
-        new['TRADE_DT'] = old['TRADE_DT']
-        new = pd.merge(new,trade_data,how = 'left',on = ['TRADE_DT','S_INFO_WINDCODE'])
-        adj_temp = old['S_DQ_CLOSE'] / new['S_DQ_CLOSE']
-
-        adj_temp.index=range(1,(len(adj_temp)+1))
-        adj_temp.ix[0]=1.0
-        adj_temp = adj_temp.sort_index()
-        adj_temp = adj_temp.cumprod()
-        new.index=range(1,(len(new)+1))
-        new.ix[0]=None
-        new['S_INFO_WINDCODE'].ix[0] = code_first
-        new['TRADE_DT'].ix[0] = day_first
-        new.ix[1:(len(new)-1),['TRADE_DT']] = date_record
-        new=new.sort_index()
-        new['adj_factor'] = adj_temp
-        new = new[['TRADE_DT','S_INFO_WINDCODE','adj_factor']]
-        data = pd.merge(new,data,how = 'right',on = ['S_INFO_WINDCODE','TRADE_DT'])
-        data = data.sort_values(by = ['TRADE_DT'])
-        data['adj_factor'] = data['adj_factor'].fillna(method = 'ffill')
-        return data
-
-    def save_data(self,path):
-        dom_data.to_hdf(path,'dom_data')
-        sub_data.to_hdf(path,'sub_data')
-
+    def save_code(self,path):
+        dom_code,sub_code = self.get_code()
+        dom_code.to_hdf(path,'dom_code')
+        sub_code.to_hdf(path,'sub_code')
 
 if __name__  ==  '__main__':
     a = GetFutureData('SR','20170101','20171231')
     trade_data = a.get_trade_data()
-    dom_data = a.get_dom_data()
-    sub_data = a.get_sub_data()
-    # a.save_data('C:\\users\\user\\Desktop\\out1.h5')
+    dom_code, sub_code = a.get_code()
+    # a.save_code('C:\\users\\user\\Desktop\\out1.h5')
--- test.py
+++ test.py
@@ -4,7 +4,12 @@ import numpy as np
 import h5py
 import re
 
-# CCommodityFuturesEODPrices：date vt preSettle open high low close volumn openinterest
+CFEcode = ('IF','IC','IH','TF','T')
+SHFcode = ('cu','al','zn','ru','fu','au','ag','rb','wr','pb','bu','hc','NI','SN')
+DCEcode = ('a','b','m','c','y','p','l','v','j','jm','i','jd','fb','bb','pp','cs')
+CZCcode = ('PM','WH','CF','SR','OI','TA','RI','LR','MA','FG','RS','RM','TC','ZC','JR','SF','SM')
+Allcode = CFEcode + SHFcode + DCEcode + CZCcode
+# CCommodityFuturesEODPrices：date vt preSettle open high low close volumn openintereST
 class GetFutureData:
 
     def __init__(self,vt,startdate,enddate):
@@ -13,33 +18,17 @@ class GetFutureData:
         self.enddate = enddate
         db = cx_Oracle.connect('fe','fe','192.168.100.22:1521/winddb')
         self.cursor = db.cursor()
-
-    def define_ExchMarktList(self):
-        '''定义不同品种所属列表'''
-        exchmarkt = {}
-        exchmarkt['CFE'] = 'filesync.CIndexFuturesEODPrices'         # 中金所
-        exchmarkt['SHF'] = 'filesync.CCommodityFuturesEODPrices'     # 上期所
-        exchmarkt['CZC'] = 'filesync.CCommodityFuturesEODPrices'     # 郑商所
-        exchmarkt['DCE'] = 'filesync.CCommodityFuturesEODPrices'     # 大商所
-        sql = '''select  s_info_windcode
-        from filesync.CFuturesDescription '''"where s_info_windcode LIKE'"+self.vt+'''%' and rownum=1'''
-        self.cursor.execute(sql)
-        t = self.cursor.fetchall()
-        return exchmarkt[t[0][0][-3:]]  #例如'IF1003.CFE'返回的是exchmarkt['CFE']的值
+        self.trade_data = self.get_trade_data()
 
     def get_trade_data(self):
-        '''读取日交易数据'''
-        sql = '''select s_info_windcode,
-        trade_dt,
-        s_dq_presettle,
-        s_dq_open,
-        s_dq_high,
-        s_dq_low,
-        s_dq_close,
-        s_dq_volume,
-        s_dq_oi from '''+self.define_ExchMarktList()+'''
-        where trade_dt>= '''+self.startdate+' and trade_dt<= '+self.enddate+"and s_info_windcode LIKE'"+self.vt+'''%'
-        and LENGTH(s_info_windcode)>=10
+        if self.vt in CFEcode:
+            exchmarkt = 'filesync.CIndexFuturesEODPrices'
+        else:
+            exchmarkt = 'filesync.CCommodityFuturesEODPrices'
+        sql = '''select s_info_windcode,trade_dt,
+        s_dq_presettle,s_dq_open,s_dq_high,s_dq_low,s_dq_close,s_dq_volume,s_dq_oi from '''+exchmarkt+'''
+        where trade_dt>= '''+self.startdate+' and trade_dt<= '+self.enddate+"and s_info_windcode LIKE '"+self.vt+'''%'
+        and LENGTH(s_info_windcode)>=9
         order by trade_dt'''
         self.cursor.execute(sql)
         trade_data = self.cursor.fetchall()
@@ -48,13 +37,48 @@ class GetFutureData:
         trade_data = trade_data.sort_values(by = ['TRADE_DT','S_INFO_WINDCODE'])
         return trade_data
 
+    def get_code(self):
+        trade_data = self.get_trade_data()
+        trade_sort = trade_data.sort_values(by = ['TRADE_DT','S_DQ_OI','S_INFO_WINDCODE'], ascending = [1,0,1])
+        maxOI = trade_sort.groupby('TRADE_DT').nth(0).reset_index()[['TRADE_DT','S_INFO_WINDCODE']]
+        subOI = trade_sort.groupby('TRADE_DT').nth(1).reset_index()[['TRADE_DT','S_INFO_WINDCODE']]
+        thiOI = trade_sort.groupby('TRADE_DT').nth(2).reset_index()[['TRADE_DT','S_INFO_WINDCODE']]
+        dom_code = maxOI
+        sub_code = subOI
+        if self.vt in CFEcode:
+            # 金融期货直接按maxOI，subOI，且不回滚 ps:windcode&code
+            sub_code[maxOI > subOI] = thiOI
+        else:
+            # 商品期货满足最大持仓量满3天
+            FSlag1check = subOI.shift(1) == maxOI
+            FSlag2check = subOI.shift(2) == maxOI
+            FScheck = [FSlag1check,FSlag2check].any(axis=1)
+            dom_code[FScheck] = None
+            dom_code = dom_code.fillna(method = 'ffill')
+            STlag1check = thiOI.shift(1) == subOI
+            STlag2check = thiOI.shift(1) == subOI
+            STcheck = [STlag1check,STlag2check].any(axis=1)
+            dom_code[STcheck] = None
+            sub_code = sub_code.fillna(method = 'ffill')
+        return dom_code sub_code
 
-a = GetFutureData('CS','20170101','20171231')
-trade_data = a.get_trade_data()
+    def get_adj_factor(self,code):
+        lead = code['S_INFO_WINDCODE'].shift(-1) != code['S_INFO_WINDCODE']
+        lag = code['S_INFO_WINDCODE'].shift(1) != code['S_INFO_WINDCODE']
+        temp = code[lead].merge(code['S_INFO_WINDCODE'][lag])
+        ### rename 'S_INFO_WINDCODE_x'=old 'S_INFO_WINDCODE_y'=new
+        temp.merge(self.trade_data['S_DQ_CLOSE'],left_on='old',right_on='S_INFO_WINDCODE',inplace=True)
+        temp.merge(self.trade_data['S_DQ_CLOSE'],left_on='new',right_on='S_INFO_WINDCODE',inplace=True)
+        ### rename 'S_DQ_CLOSE_x'=oldclose 'S_DQ_CLOSE_y'=newclose
+        temp['adj_factor'] = temp['oldclose'] / temp['newclose']
+        code['adj_factor'] = None
+        code[lag]['adj_factor'] = temp['adj_factor']
+        code = code.fillna(methond = 'ffill')
+        code = code.fillna(value = 1) # 第一个调整因子为1
+        return code
 
-trade_sort = trade_data.sort_values(by = ['TRADE_DT','S_DQ_OI','S_INFO_WINDCODE'], ascending = [1,0,1])
-maxOI = trade_sort.groupby('TRADE_DT').nth(0).reset_index()[['TRADE_DT','S_INFO_WINDCODE']]
-subOI = trade_sort.groupby('TRADE_DT').nth(1).reset_index()[['TRADE_DT','S_INFO_WINDCODE']]
-
-
-    
+if __name__  ==  '__main__':
+    a = GetFutureData('SR','20170101','20171231')
+    trade_data = a.get_trade_data()
+    dom_code, sub_code = a.get_code()
+    # a.save_code('C:\\users\\user\\Desktop\\out1.h5')
