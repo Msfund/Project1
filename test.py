6254e288c66d677b6aa36d473c9b8e895f0c648e
Shell <38464985+xyshell@users.noreply.github.com>
2018-05-10 11:50:32 +0800

    .

--- test.py
+++ test.py
@@ -3,11 +3,18 @@ from HdfUtility import *
 from FutureTickData import * 
 from HisDayData import *
 
+# 从万德获取日度交易数据写入HDF
 a = HisDayData()
-a.getData()
-
+a.getData(is_save_stitch=True)
+# 从本地获取高频数据写入HDF
 a = HisFutureTick('F:\\data_hft','C:\\Users\\user\\GitHub\\Project1\\out.hdf5','CFE')
 a.packedTick2Bar()
 
+# HDF读
 hdf=HdfUtility()
-x = hdf.hdfRead('C:\\Users\\user\\GitHub\\Project1\\out.hdf5','CFE','IC','Rawdata',None,'60m')
+# 读各个频率的Rawdata: kind1='Rawdata',kind2=None,kind3='1d'
+x = hdf.hdfRead('C:\\Users\\user\\GitHub\\Project1\\out.hdf5','CFE','IC','Rawdata',None,'1d')
+# 读StitchRule:       kind1='Stitch', kind2='00',kind3=None
+x = hdf.hdfRead('C:\\Users\\user\\GitHub\\Project1\\out.hdf5','CFE','IC','Stitch','00',None)
+# 读STitchData:       kind1='Stitch', kind2='00',kind3='1d'
+x = hdf.hdfRead('C:\\Users\\user\\GitHub\\Project1\\out.hdf5','CFE','IC','Stitch','00','1d')
